8491b3cf2c2372220baaf83b24fd9e5359ec3ceb
Michael Norrie <mike.ski247@gmail.com>
2019-05-11 19:41:15 +0200

    Google maps location

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_API_KEY
+  }
+})
 
 new Vue({
   router,
