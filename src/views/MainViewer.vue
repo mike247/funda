cb4f0f3d2c1cdeafebfba7390cfa4acae3c6fdf4
Michael Norrie <mike.ski247@gmail.com>
2019-05-11 15:32:27 +0200

    Get house api and button, with cors errors

---
+++ src/views/MainViewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout justify-center align-center column>
+      <div>Hello World</div>
+      <v-btn color="info" dark large @click="getHouseData">Get House Data</v-btn>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'MainViewer',
+  methods: {
+    getHouseData: function () {
+      this.$store.dispatch('getHouseData')
+    }
+  }
+}
+</script>
