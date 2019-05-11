1ea8363b8ac6e0bf31bd9e5424eef4a08cb23bc1
Michael Norrie <mike.ski247@gmail.com>
2019-05-11 18:00:28 +0200

    Add post css, plus a button and form to alter the House ID

--- src/views/MainViewer.vue
+++ src/views/MainViewer.vue
@@ -1,19 +1,45 @@
 <template>
   <v-container fluid fill-height>
     <v-layout justify-center align-center column>
-      <div>Hello World</div>
+      <h1 class="display-5">Search for a house</h1>
+      <div class="mainViewer_form_input-size">
+        <v-form>
+          <v-text-field label="Regular" v-model="houseId"></v-text-field>
+        </v-form>
+      </div>
       <v-btn color="info" dark large @click="getHouseData">Get House Data</v-btn>
+      <p>{{houseData}}</p>
     </v-layout>
   </v-container>
 </template>
+<style scoped>
+  .mainViewer_form_input-size {
+    width: 300px;
+  }
+</style>
 
 <script>
-export default {
-  name: 'MainViewer',
-  methods: {
-    getHouseData: function () {
-      this.$store.dispatch('getHouseData')
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'MainViewer',
+    computed: {
+      ...mapState({
+        houseData: state => state.houseData
+      }),
+      houseId: {
+        get () {
+          return this.$store.state.houseId
+        },
+        set (value) {
+          this.$store.dispatch('updateHouseId', value)
+        }
+      }
+    },
+    methods: {
+      getHouseData: function () {
+        this.$store.dispatch('getHouseData')
+      }
     }
   }
-}
 </script>
